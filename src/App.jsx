644e89a61a35f08460d396c3b0674e75f94b2864
Muhammad Eid <ieiiiid2@gmail.com>
2024-11-12 01:19:11 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+
+
+function App() {
+  return (
+    <Routes>
+    <Route path="/"></Route>
+    </Routes>
+  );
+}
+
+export default App;
