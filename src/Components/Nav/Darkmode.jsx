4248ccd7637e5b7684a11b54814cc0202b1788c3
Muhammad Eid <ieiiiid2@gmail.com>
2025-03-12 21:36:13 +0200

    light and dark mode

--- src/Components/Nav/Darkmode.jsx
+++ src/Components/Nav/Darkmode.jsx
@@ -1,36 +1,41 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { MdLightMode } from "react-icons/md";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 const Darkmode = () => {
-    const [theme, setTheme] = useState(
-        localStorage.getItem("theme") ? localStorage.getItem("theme") : "dark")
-        const element = document.documentElement;
-    useEffect(()=>{
-        if (theme == "dark") {
-            element.classList.add("dark");
-            localStorage.setItem("theme", "dark")
-        } else {
-            element.classList.remove("dark");
-            localStorage.setItem("theme", "light")
-        }
-        
-    },
-    [theme])
-     
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+  const element = document.documentElement;
+  useEffect(() => {
+    if (theme == "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+
   return (
     <>
-    
-    <MdLightMode className={theme == "light" ? "block cursor-pointer" : "hidden"} fill='#FDB813' size={32} onClick={()=> {
-        setTheme("dark")
-      }} />
-    <BsFillMoonStarsFill className={theme == "dark" ? "block cursor-pointer " : "hidden"} fill='#F6F1D5' size={32} onClick={()=> {
-        setTheme("light")
-      }}  />
-
-
-      
+      <MdLightMode
+        className={theme == "light" ? "block cursor-pointer" : "hidden"}
+        fill="#FDB813"
+        size={32}
+        onClick={() => {
+          setTheme("dark");
+        }}
+      />
+      <BsFillMoonStarsFill
+        className={theme == "dark" ? "block cursor-pointer " : "hidden"}
+        fill="#F6F1D5"
+        size={32}
+        onClick={() => {
+          setTheme("light");
+        }}
+      />
     </>
-  )
-}
+  );
+};
 
-export default Darkmode
+export default Darkmode;
